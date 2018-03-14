aa46a96b3b51588a11d9af541d762c7b737125fd
mai <168496714@qq.com>
2018-03-14 11:28:00 +0800

    Removed default config

--- config/config.default.js
+++ config/config.default.js
@@ -8,9 +8,9 @@
 exports.mongolass = {
   app: true,
   client: {
-    host: '127.0.0.1',
-    port: '27017',
-    database: 'test',
+    host: '',
+    port: '',
+    database: '',
     customPlugins: false,
   },
 };
